1d47f2a696831de84a33214835f9c20f0748d355
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-12-09 19:56:20 -0800

    10 minute auto commit

--- final.cpp
+++ final.cpp
@@ -5,6 +5,8 @@
 #include <random>
 #include <chrono>
 
+#include <sstream>
+
 #include "LinkedList.h" // Including custom data structure
 
 using namespace std;
@@ -15,16 +17,24 @@ using namespace std;
 mt19937& getGenerator();
 int generateRandomInt(int, int);
 
-struct CofeeBoothSimulator {
-    LinkedList<string, string> queue;
+struct Booth {
+    // public:
+    virtual void initializeQueue() = 0; // Pure virtual function
+    virtual string simulateRonud() = 0; // Pure virtual function
+    // virtual ~Booth() {}
+    
+};
 
+struct CoffeeBooth : public Booth {
+    // private: (make private later)
+
+    LinkedList<string, string> queue;
     vector<string> names = {
         "Alice", "Bob", "Charlie", "Diana", "Eve",
         "Frank", "Grace", "Hank", "Isabella", "Jack",
         "Karen", "Liam", "Mia", "Nathan", "Olivia",
         "Paul", "Quincy", "Rachel", "Sophia", "Tom"
     };
-
     vector<string> drinks = {
         "Coffee", "Tea", "Latte", "Mocha", "Espresso",
         "Cappuccino", "Americano", "Macchiato", "Flat White", "Affogato",
@@ -32,57 +42,67 @@ struct CofeeBoothSimulator {
         "Iced Latte", "Smoothie", "Matcha", "Turmeric Latte", "Black Tea"
     };
 
+    // public:    
     void addCustomer() {
         string name = names[generateRandomInt(0,names.size() - 1)];
         string drink = drinks[generateRandomInt(0,names.size() - 1)];
         queue.enqueue(name, drink); // Adding the customer to the queue
     }
 
-    void initializeQueue() {
+    void initializeQueue() override{
         for (int i = 0; i < 3; i++) { // Adds 3 customers to the queue
             this->addCustomer();
         }   
-        cout << "\n--- Beginning Simulation ---\n";
-        cout << "Initial queue state:\n";
-        queue.print();
     }
+    string simulateRonud() override {
+        stringstream RoundResult;
+        RoundResult << "Coffee Booth: \n";
 
-    void run() {
-        int rounds = 10;
-        for (int i = 0; i < rounds; i++) {
-            cout << "\n===== Round " << i + 1 << " =====\n";
-
-            // Server the customer at the head of the queue
-            if (!queue.isEmpty()) {
-                cout << "Serving customer: [Name: " << queue.peekFirst() 
-                    << ", Drink: " << queue.peekSecond() << "]\n";
-                queue.dequeue(); // remove the person from the queue
-            } else {
-                cout << "Queue is empty. No one to serve.\n";
-            }
-
-            // 50% chance of a new customer joining the queue
-            if (generateRandomInt(0,1) == 1) {
-                cout << "A new customer has joined the queue.\n";
-                this->addCustomer();
-            } else {cout << "No new customers joined this round.\n";}
-
-            // Printing the queue after the end of the round
-            cout << "Queue at the end of round " << i + 1 << ":\n";
-            queue.print();
+        if (!queue.isEmpty()) {
+            RoundResult << "Serving: [Name: " << queue.peekFirst()
+                   << ", Drink: " << queue.peekSecond() << "]\n";
         }
-        cout << "\n--- Simulation Complete ---\n";        
     }
-
 };
-//struct MuffinBoothSimulator
 
+
+
+// struct CofeeBoothSimulator {
+
+//     void run() {
+//         int rounds = 10;
+//         for (int i = 0; i < rounds; i++) {
+//             cout << "\n===== Round " << i + 1 << " =====\n";
+
+//             // Server the customer at the head of the queue
+//             if (!queue.isEmpty()) {
+//                 cout << "Serving customer: [Name: " << queue.peekFirst() 
+//                     << ", Drink: " << queue.peekSecond() << "]\n";
+//                 queue.dequeue(); // remove the person from the queue
+//             } else {
+//                 cout << "Queue is empty. No one to serve.\n";
+//             }
+
+//             // 50% chance of a new customer joining the queue
+//             if (generateRandomInt(0,1) == 1) {
+//                 cout << "A new customer has joined the queue.\n";
+//                 this->addCustomer();
+//             } else {cout << "No new customers joined this round.\n";}
+
+//             // Printing the queue after the end of the round
+//             cout << "Queue at the end of round " << i + 1 << ":\n";
+//             queue.print();
+//         }
+//         cout << "\n--- Simulation Complete ---\n";        
+//     }
+
+// };
 
 int main() {
     
-    CofeeBoothSimulator sim;
-    sim.initializeQueue();
-    sim.run();
+    // CofeeBoothSimulator sim;
+    // sim.initializeQueue();
+    // sim.run();
 
 
     return 0;
