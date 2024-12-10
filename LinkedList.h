152cee6bc84af845c90c4af94a1a9993a5a333f7
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-12-09 19:05:40 -0800

    10 minute auto commit

--- LinkedList.h
+++ LinkedList.h
@@ -1,6 +1,7 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+// Queue
 
 template <typename T, typename D>
 class LinkedList {
@@ -9,8 +10,8 @@ class LinkedList {
         T first;
         D second;
         Node* next;
-        Node(T data, D dataTwo) {
-            this->first = data;
+        Node(T dataOne, D dataTwo) {
+            this->first = dataOne;
             this->second = dataTwo;
             next = nullptr;
         }
@@ -25,16 +26,37 @@ class LinkedList {
     LinkedList(); 
     ~LinkedList() {clear();}
 
-    // Setters
-    void pushFront(T&, D&);
-
+    // Container methods
+    void enqueue(T&, D&); // Adds a element to the front of the queue
+    void dequeue(); // Removes the frist element from the queue
+    T peek(); // returns the value of the front element in the queue
+    bool isEmpty() {return (head == nullptr);}
+    int size() {return CurrentSize;}
+    void clear();
 
 };
 
 
 #endif
 
+// Default Constructor function
+template <typename T, typename D>
+LinkedList<T,D>::LinkedList() {
+    this->head = nullptr;
+    this->CurrentSize = 0;
+}
 
-/*
-TODO
-*/
+template <typename T, typename D>
+void LinkedList<T,D>::enqueue(T&, D&) {
+    Node* node = new Node(dataOne, dataTwo);
+    if(this->isEmpty()) { // If the queue is empty
+        head = node; // the element added is now at the front (i.e. is the head)
+    } else {
+        Node* temp = head;
+        while(temp->next != nullptr) { // traveres through queue until recahing the last element
+            temp = temp->next; 
+        }
+        temp->next = node;
+    }
+}
+
